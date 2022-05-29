18ce297e2bd35642104538024138393ed62c3664
haesa <pd6156@sookmyung.ac.kr>
2022-05-29 15:50:00 +0900

    study 5/29

--- src/App.js
+++ src/App.js
@@ -1,85 +1,92 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [pw, setPw] = useState('');
-  const [query, setQuery] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [message, setMessage] = useState("");
   const [emailValid, setEmailValid] = useState(false);
+  // const [data, setData] = useState({});
+  const reg =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 
   const onChange = (event) => {
-    if(event.target.className === 'email'){
+    if (event.target.className === "email") {
       setEmail(event.target.value);
-      const reg = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
       setEmailValid(reg.test(email));
-    } else if(event.target.className === 'password'){
+    } else if (event.target.className === "password") {
       setPw(event.target.value);
     }
   };
+
   console.log(`email: ${email}, pw: ${pw}`);
+
+  const getData = async () => {
+    const query = `mutation{
+      login (email: "${email}", password: "${pw}") {
+        token
+        user{
+          id
+          name
+          email
+        }
+      }
+    }`;
+
+    const data = await (
+      await fetch("https://graphql-ts.vercel.app/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query }),
+      })
+    ).json();
+    console.log(data);
+
+    return data;
+  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    
-    setQuery(
-      `mutation{
-        login (email: "${email}", password: "${pw}") {
-          token
-          user{
-            id
-            name
-            email
-          }
-        }
-      }`
-    );
-    const data = await (
-      await fetch('https://graphql-ts.vercel.app/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({query}),
-      })
-    ).json();
-
+    const data = await getData();
     console.log(data);
-
-    const msg = document.querySelector('.msg');
-    if(data.data === null) {
-      if(data.errors[0].message === 'No Such User Found') {
-        setMessage('아이디가 잘못되었습니다.');
-      } else if(data.errors[0].message === 'Invalid Password') {
-        setMessage('비밀번호가 잘못되었습니다.');
+    const msg = document.querySelector(".msg");
+    if (data.data === null) {
+      if (data.errors[0].message === "No Such User Found") {
+        setMessage("아이디가 잘못되었습니다.");
+      } else if (data.errors[0].message === "Invalid Password") {
+        setMessage("비밀번호가 잘못되었습니다.");
       }
-      msg.classList.add('fail');
+      msg.classList.add("fail");
     } else {
       setMessage(`Hi! ${data.data.login.user.name}`);
-      const form = document.querySelector('.form');
-      form.classList.add('success');
-      msg.classList.add('success');
+      const form = document.querySelector(".form");
+      form.classList.add("success");
+      msg.classList.add("success");
     }
   };
-  
+
   return (
     <div className="login">
       <form onSubmit={onSubmit} onChange={onChange} className="form">
-        <input 
+        <input
           className="email"
-          type="email" 
-          placeholder="Email" 
-          required 
-          autoFocus/>
-        <input 
-          className="password"
-          type="password"
-          placeholder="Password"
-          style={{
-            display: `${emailValid ? 'inline' : 'none'}`,
-          }}
-          />
-        <button>LOGIN</button>
+          type="email"
+          placeholder="Email"
+          required
+          autoFocus
+        />
+        {emailValid === true && (
+          <>
+            <input
+              className="password"
+              type="password"
+              placeholder="Password"
+            />
+            <button>LOGIN</button>
+          </>
+        )}
       </form>
       <p className="msg">{message}</p>
     </div>
